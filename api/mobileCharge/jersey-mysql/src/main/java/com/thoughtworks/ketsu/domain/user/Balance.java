541e72b6a539c1b89e2130611ee3e9e851f23924
HFCherish <hf_cherish@163.com>
2016-11-29 17:49:02 +0800

    use bifunction instead

--- api/mobileCharge/jersey-mysql/src/main/java/com/thoughtworks/ketsu/domain/user/Balance.java
+++ api/mobileCharge/jersey-mysql/src/main/java/com/thoughtworks/ketsu/domain/user/Balance.java
@@ -1,18 +1,32 @@
 package com.thoughtworks.ketsu.domain.user;
 
-import java.util.function.Function;
+import java.util.HashMap;
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
 
 /**
  * Created by pzzheng on 11/29/16.
  */
 public class Balance {
-    public double charge(CommunicationRecord record, Function<CommunicationRecord, Double> chargeStrategy) {
-        return chargeStrategy.apply(record);
+    private double remainedMoney;
+    private HashMap<CallRecord.CommunicationType, Integer> freeCallMinutes;
+    private int freeMessages;
+
+    public Balance() {
+        remainedMoney = 0.0;
+        Stream.of(CallRecord.CommunicationType.values()).map(type -> freeCallMinutes.put(type, 0));
+        freeMessages = 0;
+    }
+
+    public double charge(CommunicationRecord record, BiFunction<CommunicationRecord, Balance, Double> chargeStrategy) {
+        return chargeStrategy.apply(record, this);
     }
 
     public static class ChargeStrategies {
-        public static Function<CommunicationRecord, Double> callCharge() {
-            return record -> 0.0;
+        public static BiFunction<CommunicationRecord, Balance, Double> callCharge() {
+            return (record, balance) -> {
+                return 0.0;
+            };
         }
     }
 }
