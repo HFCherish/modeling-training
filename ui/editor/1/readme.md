1b0ae82e7f67f33ea9cbff829f0be09821d21f2d
HFCherish <hf_cherish@163.com>
2016-12-03 11:08:02 +0800

    can response to every button

--- ui/editor/1/readme.md
+++ ui/editor/1/readme.md
@@ -6,4 +6,15 @@
 	1. 可以编辑title中的内容
 	1. 可以编辑text中的内容
 3. 写原本的html架子
-2. 
+
+
+# 正确的操作dom
+1. 使用javascript操作dom
+    1. 尝试parentElement获得父元素, 而不使用parentNode(应该无所谓,dom api中一般说节点主要指的就是element)
+    1. 直接对parentElement使用getElement查找子元素中的text、node节点。getElement等操作可以直接对任一元素执行, 表示在它的子元素中查找。
+    1. 使用querySelector定义条件获取节点(这是第二种常用的查询dom的方法)
+2. 使用javascript保存value
+3. 使用jquery操作dom
+4. 使用react + nodejs实现
+    1. 使用react实现静态版本
+    2. 使用nodejs保存到本地文件
--- ui/editor/1/simpleRender.js
+++ ui/editor/1/simpleRender.js
@@ -1,30 +1,72 @@
-function view() {
-    var titleNodes = document.getElementsByClassName("title");
-    // for(var i=0; i<titleNodes.length; i++) {
-    // 	titleNodes.item(i).innerHTML =
-    // }
-    Array.from(titleNodes).forEach(function (titleNode) {
-        titleNode.innerHTML = "<h1>my test title</h1>";
-    });
-
-    var textNodes = document.getElementsByClassName("text");
-    Array.from(textNodes).forEach(function (textNode) {
-        textNode.innerHTML = "<textarea readonly>my test textarea</textarea>";
-    })
-    // while(titleNode.firstChild) {
-    // 	titleNode.removeChild(titleNode.firstChild);
-    // }
-    // titleNode.appendChild()
-}
-
 function render() {
-    var titleNodes = document.getElementsByClassName("title");
-    Array.from(titleNodes).forEach(function (titleNode) {
-        titleNode.innerHTML = "<input type='text'>";
-    });
-
-    var textNodes = document.getElementsByClassName("text");
-    Array.from(textNodes).forEach(function (textNode) {
-        textNode.innerHTML = "<textarea></textarea>";
-    });
+    var showButtons = document.getElementsByClassName("showButton");
+    for (var i = 0; i < showButtons.length; i++) {
+        var showButton = showButtons[i];
+        showButton.onclick = function () {
+            var parentNode = this.parentNode;
+            var titleNode = parentNode.querySelector('li.title');
+            var textNode = parentNode.querySelector('li.text');
+            titleNode.innerHTML = "<h1> my test title </h1>";
+            textNode.innerHTML = "<textarea readonly>my test area</textarea>";
+        };
+    }
 }
+
+function edit() {
+    var editButtons = document.getElementsByClassName("editButton");
+    for (var i = 0; i < editButtons.length; i++) {
+        editButtons[i].onclick = function () {
+            var parentNode = this.parentNode;
+            var titleNode = parentNode.querySelector('li.title');
+            var textNode = parentNode.querySelector('li.text');
+            titleNode.innerHTML = "<input type='text' />"
+            textNode.innerHTML = "<textarea></textarea>";
+        }
+    }
+}
+
+render();
+edit();
+
+// function render() {
+//     var viewButtons = document.getElementsByClassName("showButton");
+//     for(var i=0; i<viewButtons.length; i++) {
+//         var viewButton = viewButtons[i];
+//         viewButton.addEventListener('click', function (event) {
+//             var titleNode = viewButton.parentNode.childNodes.item(2);
+//             console.log(titleNode.innerHTML + "******");
+//             titleNode.innerHTML = "<h1>my test title</h1>";
+//
+//             var textNode = viewButton.parentNode.childNodes.item(3);
+//             textNode.innerHTML = "<textarea readonly>my test textarea</textarea>";
+//         })
+//
+//     };
+//     // Array.from(viewButtons).forEach(function (viewButton) {
+//     //     viewButton.addEventListener('click', function (event) {
+//     //         var titleNode = viewButton.parentNode.childNodes.item(2);
+//     //         titleNode.innerHTML = "<h1>my test title</h1>";
+//     //
+//     //         var textNode = viewButton.parentNode.childNodes.item(3);
+//     //         textNode.innerHTML = "<textarea readonly>my test textarea</textarea>";
+//     //     })
+//     // });
+// }
+//
+// function edit() {
+//     var editButtons = document.getElementsByClassName("editButton");
+//     // Array.from(editButtons).forEach(function (editButton) {
+//     for(var i=0; i<editButtons.length; i++) {
+//         var editButton = editButtons[i];
+//         editButton.addEventListener('click', function (event) {
+//             var titleNode = editButton.parentNode.childNodes.item(2);
+//             titleNode.innerHTML = "<input type='text'>";
+//
+//             var textNode = editButton.parentNode.childNodes.item(3);
+//             textNode.innerHTML = "<textarea></textarea>";
+//         })
+//     };
+// }
+//
+// render();
+// edit();
