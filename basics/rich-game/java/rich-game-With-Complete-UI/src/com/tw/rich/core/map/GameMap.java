7f45c036eed615c53859cc2e9f603e079dd30cb1
HFCherish <hf_cherish@163.com>
2016-12-05 16:29:47 +0800

    rich game: can not set tool if condition unsatisfied

--- basics/rich-game/java/rich-game-With-Complete-UI/src/com/tw/rich/core/map/GameMap.java
+++ basics/rich-game/java/rich-game-With-Complete-UI/src/com/tw/rich/core/map/GameMap.java
@@ -3,7 +3,9 @@ package com.tw.rich.core.map;
 import com.tw.rich.core.assistenceItems.Tool;
 import com.tw.rich.core.places.Hospital;
 import com.tw.rich.core.places.Place;
+import com.tw.rich.core.player.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -16,11 +18,13 @@ public class GameMap {
     private final int width;
     private final int height;
     private final List<Place> places;
+    private List<Player> players;
 
     public GameMap(int width, int height, Place... places) {
         this.width = width;
         this.height = height;
         this.places = asList(places);
+        players = new ArrayList<>();
     }
 
     public Place move(Place start, int steps) {
@@ -45,14 +49,22 @@ public class GameMap {
     }
 
     public boolean setTool(Tool tool, Place start, int steps) {
-        if(!tool.equals(Tool.BLOCK) && !tool.equals(Tool.BOMB)) return false;
-        int startIndex = places.indexOf(start);
-        int targetIndex = nextIndex(startIndex, steps);
-        places.get(targetIndex).setTool(tool);
+        if((!tool.equals(Tool.BLOCK) && !tool.equals(Tool.BOMB)) || Math.abs(steps) > 10) return false;
+
+        Place targetPlace = places.get(nextIndex(places.indexOf(start), steps));
+        if(targetPlace.getTool() != null || players.stream().filter(p -> p.currentPlace().equals(targetPlace)).count() > 0) {
+            return false;
+        }
+        targetPlace.setTool(tool);
         return true;
     }
 
     public boolean useRobot(Place start) {
         return false;
+    }
+
+
+    public void initPlayers(Player... players) {
+        this.players = asList(players);
     }
 }
--- basics/rich-game/java/rich-game-With-Complete-UI/test/com/tw/rich/core/map/GameMapTest.java
+++ basics/rich-game/java/rich-game-With-Complete-UI/test/com/tw/rich/core/map/GameMapTest.java
@@ -1,13 +1,16 @@
 package com.tw.rich.core.map;
 
+import com.tw.rich.core.Game;
 import com.tw.rich.core.assistenceItems.Tool;
 import com.tw.rich.core.places.Estate;
 import com.tw.rich.core.places.Starting;
+import com.tw.rich.core.player.Player;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by pzzheng on 11/28/16.
@@ -44,7 +47,7 @@ public class GameMapTest {
     }
 
     @Test
-    public void should_able_to_put_block_or_bomb() {
+    public void should_able_to_set_block_or_bomb() {
         Starting starting = new Starting();
         Estate estate1 = new Estate(10);
         Estate estate2 = new Estate(10);
@@ -54,5 +57,27 @@ public class GameMapTest {
         assertThat(estate2.getTool(), is(nullValue()));
         assertThat(map.setTool(Tool.BLOCK, starting, 2), is(true));
         assertThat(estate2.getTool(), is(Tool.BLOCK));
+    }
+
+    @Test
+    public void should_not_set_block_or_bomb_if_condition_unsatisfied() {
+        Starting starting = new Starting();
+        Estate estate1 = new Estate(10);
+        Estate estate2 = new Estate(10);
+        Estate estate3 = new Estate(10);
+        GameMap map = new GameMap(2, 2, starting, estate1, estate2, estate3);
+
+        assertThat(map.setTool(Tool.BLOCK, starting, 14), is(false));
+        assertThat(estate2.getTool(), is(nullValue()));
+
+        Player player = Player.createPlayerWithFund_Game_Command_State(1000, mock(Game.class));
+        player.moveTo(estate2);
+        map.initPlayers(player);
+        assertThat(map.setTool(Tool.BLOCK, starting, 2), is(false));
+        assertThat(estate2.getTool(), is(nullValue()));
+
+        estate3.setTool(Tool.BLOCK);
+        assertThat(map.setTool(Tool.BLOCK, starting, 3), is(false));
+        assertThat(estate3.getTool(), is(Tool.BLOCK));
     }
 }
